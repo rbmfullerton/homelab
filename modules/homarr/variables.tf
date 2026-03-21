d46f9cea73d5b2fe906894a7aa14a322431ef249
rbmfullerton <rbmfullerton@hotmail.com>
2026-03-21 13:48:25 -0400

    Update variables.tf

--- modules/homarr/variables.tf
+++ modules/homarr/variables.tf
@@ -30,7 +30,7 @@ variable "image" {
 variable "image_version" {
   description = "Version of container image"
   type        = string
-  default     = "v1.56.1"
+  default     = "v1.56.0"
 }
 
 variable "mount_path" {
