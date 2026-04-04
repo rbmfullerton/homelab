7fa4f35a6bd581ce9c363d94faae16e20d68837f
rbmfullerton <rbmfullerton@hotmail.com>
2026-04-04 08:31:17 -0400

    Change default value of mount_name2 variable

--- modules/bazarr/variables.tf
+++ modules/bazarr/variables.tf
@@ -54,7 +54,7 @@ variable "mount_path3" {
 variable "mount_name2" {
   description = "Mount Path for container's data"
   type        = string
-  default     = "smb-tvshows-internal"
+  default     = "smb-tvshows-internal-backup"
 }
 
 variable "mount_name3" {
